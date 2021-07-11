94b3f3f2664d495982267c71461a6059e9e2c495
unknown <vargeo@hotmail.com>
2021-07-11 16:43:57 +0300

    iot-device-app final

---
+++ app.js
@@ -0,0 +1,43 @@
+const awsIot = require('aws-iot-device-sdk');
+const epochTime = Math.floor(new Date().getTime() / 1000);
+require('dotenv').config();
+
+
+let sensor_measurements = {
+    "metadata": {
+        "gateway_ip"  : "3108432946",
+        "labelpartner_id": "npower"
+    },
+    "results" : [
+        {
+            "timestamp": {
+                "seconds"     : epochTime,
+                "microseconds": 0
+            },
+            "value"    : 300 + Math.floor(Math.random() * 700)
+        }
+    ]
+};
+
+const clientId = sensor_measurements.metadata.labelpartner_id;
+
+const device = awsIot.device({
+    keyPath: process.env.DEVICE_KEYPATH,
+        certPath: process.env.DEVICE_CERTPATH,
+            caPath: process.env.DEVICE_CAPATH,
+                clientId: process.env.DEVICE_CLIENT_ID,
+                    host: process.env.HOST
+                        });
+                        device
+                        .on('connect', function() {
+                        console.log('connect');
+                        // device.subscribe('topic_1');
+                        device.publish('device/' + process.env.DEVICE_CLIENT_ID, JSON.stringify(sensor_measurements),null,() => device.end());
+                        // console.log('after publish');
+                    });
+
+                        device
+                        .on('message', function(topic, payload) {
+                        console.log('message', topic, payload.toString());
+                    });
+
